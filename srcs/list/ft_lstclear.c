f44d2b1626a13bbcf94a11716600a5f1c34bbf9f
mohazerro <mohazerr@Pirate.local>
2022-09-20 04:43:59 +0200

    lstclear jv dormir

--- srcs/list/ft_lstclear.c
+++ srcs/list/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: mohazerr <mohazerr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 04:28:15 by mohazerr          #+#    #+#             */
-/*   Updated: 2022/09/20 04:36:30 by mohazerr         ###   ########.fr       */
+/*   Updated: 2022/09/20 04:43:22 by mohazerr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
+	t_list	*temp;
+	t_list	*cplst;
+
 	if (lst == NULL || del == NULL)
 		return ;
+	cplst = *(lst);
+	while (cplst != NULL)
+	{
+		(*del)(cplst->content);
+		temp = cplst->next;
+		free(cplst);
+		cplst = temp;
+	}
+	*lst = NULL;
 }
