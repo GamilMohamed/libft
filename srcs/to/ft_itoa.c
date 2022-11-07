f43ecadb23d3859f3a224c2044e7be8e1515cd55
mohazerro <mohazerr@Pirate.local>
2022-11-08 00:29:17 +0100

    itoa

--- srcs/to/ft_itoa.c
+++ srcs/to/ft_itoa.c
@@ -3,90 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mohazerr <mohazerr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mgamil <mgamil@42.student.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 12:38:03 by mohazerr          #+#    #+#             */
-/*   Updated: 2022/09/20 03:34:12 by mohazerr         ###   ########.fr       */
+/*   Updated: 2022/11/08 00:27:37 by mgamil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strcpy(char *s1, char *s2)
-{
-	int	i;
-
-	i = -1;
-	while (s2[++i])
-		s1[i] = s2[i];
-	s1[i] = '\0';
-	return (s1);
-}
-
-int	calcsize(int nb)
-{
-	int	i;
-
-	i = 0;
-	if (nb < 0)
-	{
-		nb *= -1;
-		i++;
-	}
-	while (nb > 0)
-	{
-		nb /= 10;
-		i++;
-	}
-	return (i);
-}
-
-char	*itoarray(int n, char *var)
+int	ft_nblen(int n)
 {
 	int	len;
-	int	i;
 
-	i = 0;
-	len = calcsize(n) - 1;
-	if (n < 0)
+	if (n == 0)
+		return (1);
+	len = (n < 0);
+	while (n != 0)
 	{
-		n *= -1;
-		var[0] = '-';
-		i = 1;
-		var[len + 1] = '\0';
-	}
-	else
-		var[len] = '\0';
-	while (i <= len)
-	{
-		var[len] = n % 10 + '0';
 		n /= 10;
-		len--;
+		len++;
 	}
-	return (var);
+	return (len);
 }
 
 char	*ft_itoa(int n)
 {
-	char	*var;
+	int				i;
+	int				len;
+	unsigned int	nb;
+	char			*str;
 
-	if (n == -2147483648)
-	{
-		var = malloc(sizeof(char) * ft_strlen("-2147483648") + 1);
-		var = ft_strcpy(var, "-2147483648");
-		var[ft_strlen("-2147483648") + 1] = '\0';
-		return (var);
-	}
-	if (n == 0)
-	{
-		var = malloc(sizeof(char) * 2);
-		var[0] = '0';
-		var[1] = '\0';
-		return (var);
-	}
-	var = malloc(sizeof(char) * calcsize(n) + 1);
-	if (!var)
+	nb = -(n < 0) * (n) + (n > 0) * (n);
+	i = (n < 0);
+	len = ft_nblen(n);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
 		return (NULL);
-	var = itoarray(n, var);
-	return (var);
+	if (n < 0)
+		str[0] = '-';
+	str[len] = 0;
+	while (--len >= i)
+	{
+		str[j] = (nb % 10) + '0';
+		nb /= 10;
+	}
+	return (str);
 }
